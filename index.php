4f6d309ecf4e7884c9e58207f591ef403f15af71
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-01 13:57:48 -0700

    started to set up website with page structure and element attributes

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
 <html>
 	<head>
 		<title> I completed the prework</title>
+		<meta charset="utf-8">
 	</head>
 	<body>
-		<p>I completed the prework!</p>
+		<h1>My first website</h1>
+		<p>This is a sample website, which will teach me to work with <strong>html</strong> and <em>css</em></p>
+		<div><h2>html learning goals</h2></div>
+		<ul>
+			<li>Page Structure</li>
+			<li>Element Attributes</li>
+			<li>Tag Types</li>
+			<li>Linking in other resources</li>
+		</ul>
 	</body>
 </html>
