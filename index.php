2a941f65ba4a25ce2f3f5d0703a7623c06f8fc7a
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-01 14:12:31 -0700

    added info about CSS

--- index.php
+++ index.php
@@ -7,12 +7,23 @@
 	<body>
 		<h1>My first website</h1>
 		<p>This is a sample website, which will teach me to work with <strong>html</strong> and <em>css</em></p>
-		<div><h2>html learning goals</h2></div>
+		<div><h2>HTML Learning Goals:</h2>
 		<ul>
 			<li>Page Structure</li>
 			<li>Element Attributes</li>
 			<li>Tag Types</li>
 			<li>Linking in other resources</li>
 		</ul>
+		</div>
+		<div>
+			<h2>CSS Learning Goals:</h2>
+			<ul>
+				<li>What properties are</li>
+				<li>How you use elements, and element attributes to change styles</li>
+				<li>How CSS styles cascade</li>
+				<li>How to include these styles</li>
+			</ul>
+		</div>
+	</body>
 	</body>
 </html>
