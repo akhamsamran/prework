530b0171ed73b9f97c1ea2047d27060fb25a0294
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-02 13:37:04 -0700

    variables, data types and typing examples in javascript

---
+++ fundamentals/variables-and-data.js
@@ -0,0 +1,13 @@
+/*
+* Variables, Data Types, and Typing
+ */
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+//modulo returns the remainder
+
+let string = "String1" + "String2";
+console.log(string);
+
