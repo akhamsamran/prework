f24705ef344de87ffe8a7a0381d475b157a42c4a
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-03 11:39:57 -0700

    re-written 3rd time, now and it works, but still a fault, it is only returning the red number

--- color-picker/script.js
+++ color-picker/script.js
@@ -11,16 +11,28 @@ function initColorPicker() {
 	let colorPickers = document.getElementsByClassName("picker");
 	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
-
 function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
-	for(let i = 0; i < pickerElements.length; i++) {
+	let pickerLen = pickerElements.length;
+	for (let i = 0; i < pickerLen; i++) {
 		pickerElements[i].addEventListener('change', () => {
-			console.log("Red value : ", rgb.red.value);
-			setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value)
-	})
+			console.log("Red value: ", rgb.red.value);
+			let red = rgb.red.value;
+			let green = rgb.green.value;
+			let blue = rgb.blue.value;
+			setBoxBGColor(colorBox, red, green, blue);
+		});
 	}
 }
 function setBoxBGColor(colorBox, red, green, blue) {
-	rgbVal = [red, green, blue].join[','];
+	rgbVal = [red, green, blue].join(',');
 	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+function setDisplayValues(red, green, blue) {
+	let redVal =document.getElementById("redVal");
+	let greenVal =document.getElementById("greenVal");
+	let blueVal =document.getElementById("blueVal");
+
+	redVal.innerText = red;
+	greenVal.innerText = green;
+	blueVal.innerText = blue;
 }
