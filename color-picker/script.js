49f30a78c0f21b303b09d99c6e608cfee250e9e4
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-03 11:05:37 -0700

    re-written all over again, still doesn't work

--- color-picker/script.js
+++ color-picker/script.js
@@ -9,34 +9,18 @@ function initColorPicker() {
 		blue: document.getElementById("blue")
 	};
 	let colorPickers = document.getElementsByClassName("picker");
-	setColorPickerEventListeners(colorBox, rgb);
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
 function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
 	for(let i = 0; i < pickerElements.length; i++) {
-		pickerElements[i].addEventListener('change', () = > {
+		pickerElements[i].addEventListener('change', () => {
+			console.log("Red value : ", rgb.red.value);
 			setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value)
 	})
 	}
 }
-
-/*
-	rgb.red.addEventListener('change', () => {
-		console.log("Red value: ", rgb.red.value);
-		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-})
-	rgb.green.addEventListener('change', () => {
-		console.log("Green value: ", rgb.green.value);
-	setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-});
-	rgb.blue.addEventListener('change', () => {
-		console.log("Blue value: ", rgb.blue.value);
-	setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-});
-}
-
-function setBoxBGColor(colorBox, red, green, blue){
-	rgbVal =[red, green, blue].join[','];
+function setBoxBGColor(colorBox, red, green, blue) {
+	rgbVal = [red, green, blue].join[','];
 	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
-*/
