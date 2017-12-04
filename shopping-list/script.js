b162a8bf37c576c99bdec97a2ed6d41f8a2a4ee6
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-03 17:06:07 -0700

    javascript file mostly set up

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,1 +1,37 @@
-console.log(Test);
+window.onload = function () {
+	initShoppingList();
+};
+
+function initShoppingList() {
+	let form = document.getElementById("item-form");
+
+form.addEventListener("submit", (event) => {
+	handleItemForm(event, form);
+});
+}
+
+function handleItemForm(event, formRef) {
+	if(event.preventDefault()) {
+		event.preventDefault();
+	}
+
+	addItemToShoppingList();
+
+	return false
+}
+
+function addItemToShoppingList(){
+	let itemName = document.getElementById("item-name");
+	let itemAmount = document.getElementById("item-amount");
+	let itemHtml = createListItemHtml();
+	console.log("Item HTML: ", itemHtml);
+	let itemListRef = document.getElementById("shopping-list");
+	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+}
+
+function createListItemHtml() {
+	return `<li> 
+					Item Name - Amount
+				</li>
+	`;
+}
