6e38ab2b075f6adf77103ef9c5b40d5a39b04f35
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-23 15:34:00 -0700

    added increment and decrement examples

--- php/test1.php
+++ php/test1.php
@@ -13,3 +13,11 @@ echo "<br>";
 $current_user = $username;
 echo $current_user;
 ?>
+<?php
+$y = 0;
+if ($y-- == 0) echo $y; //output is -1 and $y value changed to -1
+?>
+<?php
+$x = 9;
+if (++$x == 10)  echo $x; //first increment the value of $x, then test to see if it is value of 10, if yes, then output the value
+?>
