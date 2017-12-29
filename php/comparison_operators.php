63513f5ef451ee4ee324d3d05963479179a41743
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-29 12:44:09 -0700

    added php example 4-16

--- php/comparison_operators.php
+++ php/comparison_operators.php
@@ -5,9 +5,21 @@
  * Date: 12/29/2017
  * Time: 12:27 PM
  */
+/* exercise 4-15*/
 $a = 2; $b = 3;
 if ($a > $b) echo "$a is greater than $b<br>";
 if ($a < $b) echo "$a is less than $b<br>";
 if ($a >= $b) echo "$a is greater than or equal to $b<br>";
-if ($a <= $b) echo "$a is less than or equal to $b"
+if ($a <= $b) echo "$a is less than or equal to $b<br>";
+
+
+
+	/*exercise 4-16*/
+
+$a = 1; $b = 0;
+echo ($a AND $b) . " (1)<br>";
+echo ($a or $b) . " (2)<br>";
+echo ($a XOR $b) . " (3)<br>";
+echo !$a . " (4)<br>";
+
 ?>
