ce26427fa56b66b3eace15d386597f28989ca3dc
Anna Khamsamran <akamsamran1@cnm.edu>
2017-12-29 12:31:32 -0700

    php comparison operators example 4-15

---
+++ php/comparison_operators.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 12/29/2017
+ * Time: 12:27 PM
+ */
+$a = 2; $b = 3;
+if ($a > $b) echo "$a is greater than $b<br>";
+if ($a < $b) echo "$a is less than $b<br>";
+if ($a >= $b) echo "$a is greater than or equal to $b<br>";
+if ($a <= $b) echo "$a is less than or equal to $b"
+?>
